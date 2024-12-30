1627cf24adb878ec3cd4994659703a118a730edc
thubten1234 <thubten219@gmail.com>
2024-12-30 23:13:32 +1100

    d

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import compression from 'vite-plugin-compression';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      filename: 'stats.html', 
+      gzipSize: true, 
+      brotliSize: true, 
+    }),
+    compression(),
+  ],
+});
