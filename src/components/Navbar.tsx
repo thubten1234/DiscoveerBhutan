c898a912a584fa15844fed64daae2848a993ff0b
thubten1234 <thubten219@gmail.com>
2024-12-30 23:33:44 +1100

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Navbar.css";
 
 function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      // Scrolling down
+      setShowNavbar(false);
+    } else {
+      // Scrolling up
+      setShowNavbar(true);
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <nav
+      className={`navbar navbar-expand-lg navbar-dark bg-dark sticky-top ${
+        showNavbar ? "show" : "hide"
+      }`}
+    >
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Discover Bhutan
